d2d11b1c240b9e5036c08295c56a7fe008aaa7cd
chanper <qianchaosolo@gmail.com>
2023-09-13 21:39:33 +0800

    update

--- example-client/src/main/java/github/javaguide/HelloController.java
+++ example-client/src/main/java/github/javaguide/HelloController.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloController {
 
-    @RpcReference(version = "1.0", group = "group1")
+    @RpcReference(version = "1.0", group = "group2")
     private HelloService helloService;
 
     public void test() throws InterruptedException {
-        String hello = this.helloService.hello(new Hello("111", "222"));
-        //如需使用 assert 断言，需要在 VM options 添加参数：-ea
-        assert "Hello description is 222".equals(hello);
+        String response = this.helloService.hello(new Hello("HelloMsg", "This is description of Hello Message"));
+        System.out.println("RPC response: " + response);
+
         Thread.sleep(12000);
         for (int i = 0; i < 10; i++) {
             System.out.println(helloService.hello(new Hello("111", "222")));
